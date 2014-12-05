815c2188b015d9c5c146d90ab8bb499c9abaa87e
imcmib <imcmib@gmail.com>
2014-12-05 20:56:26 +0200

    Lesson 18 update update.

---
+++ Lesson_18/src/main/java/org/itstep/android/lesson_18/SecondActivity.java
@@ -0,0 +1,15 @@
+package org.itstep.android.lesson_18;
+
+import android.app.Activity;
+
+/*
+ * SecondActivity.java
+ *
+ * Created by mib on 05.12.14, 20:50
+ *
+ * Copyright(c) 2014 Provectus IT Company, Inc.  All Rights Reserved.
+ * This software is the proprietary information of Provectus IT Company.
+ *
+ */
+public class SecondActivity extends Activity {
+}
