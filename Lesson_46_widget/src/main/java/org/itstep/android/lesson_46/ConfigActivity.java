bdfe4d477ad75103b8600d2ccdc77e4f296cbe02
imcmib <imcmib@gmail.com>
2015-06-03 19:22:00 +0300

    Lesson 46 update.

--- Lesson_46_widget/src/main/java/org/itstep/android/lesson_46/ConfigActivity.java
+++ Lesson_46_widget/src/main/java/org/itstep/android/lesson_46/ConfigActivity.java
@@ -14,45 +14,52 @@ public class ConfigActivity extends ActionBarActivity {
     public final static String WIDGET_TIME_FORMAT = "widget_time_format_";
     public final static String WIDGET_COUNT = "widget_count_";
 
-    int widgetID = AppWidgetManager.INVALID_APPWIDGET_ID;
-    Intent resultValue;
-    SharedPreferences sp;
-    EditText etFormat;
+    int mAppwidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
+    Intent mResultValue;
+    SharedPreferences mPreferences;
+    EditText mFormatEditText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            widgetID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+            mAppwidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
         }
-        if (widgetID == AppWidgetManager.INVALID_APPWIDGET_ID) {
+        if (mAppwidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
             finish();
         }
 
-        resultValue = new Intent();
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        mResultValue = new Intent();
+        mResultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppwidgetId);
 
-        setResult(RESULT_CANCELED, resultValue);
+        setResult(RESULT_CANCELED, mResultValue);
 
         setContentView(R.layout.activity_config);
 
-        sp = getSharedPreferences(WIDGET_PREF, MODE_PRIVATE);
-        etFormat = (EditText) findViewById(R.id.etFormat);
-        etFormat.setText(sp.getString(WIDGET_TIME_FORMAT + widgetID, "HH:mm:ss"));
+        mPreferences = getSharedPreferences(WIDGET_PREF, MODE_PRIVATE);
+        String format = mPreferences.getString(WIDGET_TIME_FORMAT + mAppwidgetId, "HH:mm:ss");
 
-        int cnt = sp.getInt(ConfigActivity.WIDGET_COUNT + widgetID, -1);
+        mFormatEditText = (EditText) findViewById(R.id.etFormat);
+        mFormatEditText.setText(format);
+
+        int cnt = mPreferences.getInt(ConfigActivity.WIDGET_COUNT + mAppwidgetId, -1);
         if (cnt == -1) {
-            sp.edit().putInt(WIDGET_COUNT + widgetID, 0);
+            mPreferences.edit()
+                        .putInt(WIDGET_COUNT + mAppwidgetId, 0)
+                        .apply();
         }
     }
 
     public void onClick(View v){
-        sp.edit().putString(WIDGET_TIME_FORMAT + widgetID, etFormat.getText().toString()).commit();
+        final String format = mFormatEditText.getText().toString();
+        mPreferences.edit()
+                    .putString(WIDGET_TIME_FORMAT + mAppwidgetId, format)
+                    .apply();
 
-        setResult(RESULT_OK, resultValue);
+        setResult(RESULT_OK, mResultValue);
         finish();
     }
 }
--- Lesson_46_widget/src/main/java/org/itstep/android/lesson_46/MyWidget.java
+++ Lesson_46_widget/src/main/java/org/itstep/android/lesson_46/MyWidget.java
@@ -16,8 +16,8 @@ public class MyWidget extends AppWidgetProvider {
 
     final static String ACTION_CHANGE = BuildConfig.APPLICATION_ID + ".ACTION_CHANGE";
 
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-                         int[] appWidgetIds) {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
         for (int i : appWidgetIds) {
@@ -25,6 +25,7 @@ public class MyWidget extends AppWidgetProvider {
         }
     }
 
+    @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
 
@@ -36,58 +37,55 @@ public class MyWidget extends AppWidgetProvider {
             editor.remove(ConfigActivity.WIDGET_COUNT + widgetID);
         }
 
-        editor.commit();
+        editor.apply();
     }
 
-    static void updateWidget(Context ctx, AppWidgetManager appWidgetManager,
-                             int widgetID) {
-        SharedPreferences sp = ctx.getSharedPreferences(
+    static void updateWidget(Context context, AppWidgetManager appWidgetManager, int widgetID) {
+        SharedPreferences sp = context.getSharedPreferences(
                 ConfigActivity.WIDGET_PREF, Context.MODE_PRIVATE);
 
-        String timeFormat = sp.getString(ConfigActivity.WIDGET_TIME_FORMAT + widgetID, null);
-        if (timeFormat == null) {
-            timeFormat = "HH:mm:ss";
-        }
+        String timeFormat = sp.getString(ConfigActivity.WIDGET_TIME_FORMAT + widgetID, "HH:mm:ss");
 
         SimpleDateFormat sdf = new SimpleDateFormat(timeFormat);
         String currentTime = sdf.format(new Date(System.currentTimeMillis()));
 
-        String count = String.valueOf(sp.getInt(ConfigActivity.WIDGET_COUNT
-                + widgetID, 0));
+        int count = sp.getInt(ConfigActivity.WIDGET_COUNT + widgetID, 0);
 
-        RemoteViews widgetView = new RemoteViews(ctx.getPackageName(),
-                R.layout.widget);
+        RemoteViews widgetView = new RemoteViews(context.getPackageName(), R.layout.widget);
         widgetView.setTextViewText(R.id.tvTime, currentTime);
-        widgetView.setTextViewText(R.id.tvCount, count);
+        widgetView.setTextViewText(R.id.tvCount, String.valueOf(count));
 
-        Intent configIntent = new Intent(ctx, ConfigActivity.class);
+        Intent configIntent = new Intent(context, ConfigActivity.class);
         configIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
         configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
-        PendingIntent pIntent = PendingIntent.getActivity(ctx, widgetID,
-                configIntent, 0);
+
+        PendingIntent pIntent = PendingIntent.getActivity(context, widgetID, configIntent, 0);
         widgetView.setOnClickPendingIntent(R.id.tvPressConfig, pIntent);
 
-        Intent updateIntent = new Intent(ctx, MyWidget.class);
+        Intent updateIntent = new Intent(context, MyWidget.class);
         updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
-                new int[] { widgetID });
-        pIntent = PendingIntent.getBroadcast(ctx, widgetID, updateIntent, 0);
+        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new int[] { widgetID });
+
+        pIntent = PendingIntent.getBroadcast(context, widgetID, updateIntent, 0);
         widgetView.setOnClickPendingIntent(R.id.tvPressUpdate, pIntent);
 
-        Intent countIntent = new Intent(ctx, MyWidget.class);
+        Intent countIntent = new Intent(context, MyWidget.class);
         countIntent.setAction(ACTION_CHANGE);
         countIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
-        pIntent = PendingIntent.getBroadcast(ctx, widgetID, countIntent, 0);
+
+        pIntent = PendingIntent.getBroadcast(context, widgetID, countIntent, 0);
         widgetView.setOnClickPendingIntent(R.id.tvPressCount, pIntent);
 
         appWidgetManager.updateAppWidget(widgetID, widgetView);
     }
 
+    @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
         if (intent.getAction().equalsIgnoreCase(ACTION_CHANGE)) {
             int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
             Bundle extras = intent.getExtras();
             if (extras != null) {
                 mAppWidgetId = extras.getInt(
@@ -95,14 +93,19 @@ public class MyWidget extends AppWidgetProvider {
                         AppWidgetManager.INVALID_APPWIDGET_ID);
 
             }
+
             if (mAppWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
                 SharedPreferences sp = context.getSharedPreferences(
                         ConfigActivity.WIDGET_PREF, Context.MODE_PRIVATE);
                 int cnt = sp.getInt(ConfigActivity.WIDGET_COUNT + mAppWidgetId,  0);
-                sp.edit().putInt(ConfigActivity.WIDGET_COUNT + mAppWidgetId, ++cnt).commit();
+                sp.edit()
+                  .putInt(ConfigActivity.WIDGET_COUNT + mAppWidgetId, ++cnt)
+                  .apply();
 
                 updateWidget(context, AppWidgetManager.getInstance(context), mAppWidgetId);
             }
         }
     }
+
+
 }
